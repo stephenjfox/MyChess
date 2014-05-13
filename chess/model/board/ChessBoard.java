31947eec28312eada6184f4f585141b6311e917e
Stephen Fox <stevemasta34@gmail.com>
2014-05-13 14:34:36 -0600

    Added the ChessBoard class
    A class to have a functional, inner array to do basic logic with the
    piece retrieval.

---
+++ chess/model/board/ChessBoard.java
@@ -0,0 +1,10 @@
+package chess.model.board;
+
+public class ChessBoard {
+	final int rowCount = 8;
+	final int columnCount = 8;
+	BoardLocation[][] functionalBoard = new BoardLocation[columnCount][rowCount];
+
+	
+	
+}
